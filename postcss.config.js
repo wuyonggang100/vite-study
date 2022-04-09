5e04fcb776449de22a5b92cf5517928939dd365d
”wuyonggang100git config --global user.name ”wuyonggang100 <408727759@qq.com>
2022-04-09 19:09:47 +0800

    feat: vite 支持 css ，支持 原生 @import

---
+++ postcss.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    plugins: [
+        require('@postcss-plugins/console') // 在编译css 文件时进行输出
+    ]
+}
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      '@styles': '/src/styles' // 第一个 / 表示项目根目录
+    }
+  }
+})
